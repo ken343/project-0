ae0c1ece4325f5be910dca6c0382ba8cdb9084d9
Ken <kmcdole21@gmail.com>
2020-01-08 16:13:58 -0600

    Adding print Restaurant Function
    Currently the function does not format the output in a likable way.
    Next step may be to add multiple restaurants to an array/slice.

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt"
 
-func init() {
-	type Resturant struct {
-		name     string
-		cuisine  string
-		price    float64
-		distance float64
-	}
+type restaurant struct {
+	name     string
+	cuisine  string
+	price    float64
+	distance float64
+}
+
+//Prints in format: [McDonalds: Fast Food | Under $10 | Under 5 miles]
+func printR(r restaurant) {
+	fmt.Println(r.name, r.cuisine, r.price, r.distance)
 }
 
 func main() {
-	var instantRestuant string = "[McDonalds: Fast Food | Under $10 | Under 5 miles]"
+
+	var mcd restaurant = restaurant{
+		name:     "McDonalds",
+		cuisine:  "fast-food",
+		price:    10,
+		distance: 5,
+	}
+
+	printR(mcd)
+
+	var instantRestuant string = "[McDona lds: Fast Food | Under $10 | Under 5 miles]"
 	fmt.Println(instantRestuant)
 }
