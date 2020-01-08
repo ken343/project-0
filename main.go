93f78c193239dd454b787422c3eb6c3f494bbc5a
Ken <kmcdole21@gmail.com>
2020-01-08 15:17:09 -0600

    Adding Structure
    Structure represents the resurant's fields. Will add function for
    printing out struction in formmatted way.

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func init() {
+	type Resturant struct {
+		name     string
+		cuisine  string
+		price    float64
+		distance float64
+	}
+}
+
 func main() {
 	var instantRestuant string = "[McDonalds: Fast Food | Under $10 | Under 5 miles]"
 	fmt.Println(instantRestuant)
