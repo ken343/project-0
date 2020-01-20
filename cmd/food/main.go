a7095cd27ebe2464d9661865e47ce84f959f44c0
Ken <kmcdole21@gmail.com>
2020-01-19 19:11:15 -0600

    Removing Restaurants Type
    This struct is no longer needed mow that it has been replaced by the
    Places type.

--- cmd/food/main.go
+++ cmd/food/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/ken343/project-0/cmd/food/internal/place"
-	"github.com/ken343/project-0/cmd/food/internal/restaurant"
 )
 
 func main() {
@@ -21,21 +20,5 @@ func main() {
 	defer resp.Body.Close()
 
 	var myPlace place.Places = loadPlaces(resp.Body)
-	//myPlace = RestaurantFilter(myPlace, *pFoodType, *pMaxPrice, *pMaxDistance)
 	fmt.Println(myPlace)
-}
-
-// RestaurantFilter will take in the command line flags and use them to pare down the array of restaurants
-// down to reasonable list.
-func RestaurantFilter(r []restaurant.Restaurant, food string, price float64, distance float64) []restaurant.Restaurant {
-	if food != "" {
-		r = restaurant.FilterCuisine(r, restaurant.EQL, food)
-	}
-	if price > 0 {
-		r = restaurant.FilterPrice(r, restaurant.LTE, price)
-	}
-	if distance > 0 {
-		r = restaurant.FilterDistance(r, restaurant.LTE, distance)
-	}
-	return r
 }
