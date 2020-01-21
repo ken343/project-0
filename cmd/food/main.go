eeeac85ecd2c376913a0390116c189ff3aa69987
Ken <kmcdole21@gmail.com>
2020-01-21 10:16:24 -0600

    Changing env greeting to work on both Windows & Linux

--- cmd/food/main.go
+++ cmd/food/main.go
@@ -5,12 +5,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 
 	"github.com/ken343/project-0/cmd/food/internal/place"
 )
 
 func main() {
-	var name string = os.Getenv("USERNAME")
+	var name string
+
+	if runtime.GOOS == "windows" {
+		name = os.Getenv("USERNAME")
+	} else {
+		name = os.Getenv("USER")
+	}
 	fmt.Printf("Howdy %s, here are your options for tonight.\n\n", name)
 
 	resp, err := http.Get(url) //url is globally located in config.go
