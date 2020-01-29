a99fecd678b6dcc834b1ef27f17eb7f8f9b94071
ken343 <kmcdole21@gmail.com>
2020-01-29 01:12:07 -0600

    Adding Loading Functions to Daemon
    The loading functions have been moved to the server. Will need to add a
    way to write back info to client.

--- cmd/foodd/foodd.go
+++ cmd/foodd/foodd.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/ken343/project-0/localpkg/myerror"
 	"github.com/ken343/project-0/localpkg/place"
 )
 
 func main() {
 	fmt.Println("Server is running on port 8080...")
 
-	http.HandleFunc("/chow", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//Retrieve food type, distance and price from client application.
-		r.Form.
-			url = place.ProduceQueryString(*pFoodType, *pMaxDistance, *pMaxPrice)
+
+		r.ParseForm()
+		foodType := r.Form.Get("food")
+		foodDistance := r.Form.Get("distance")
+		foodPrice := r.Form.Get("pricelevel")
+		floatFoodDistance, err := strconv.ParseFloat(foodDistance, 64)
+		myerror.Check(err)
+		floatFoodPrice, err := strconv.ParseFloat(foodPrice, 64)
+		myerror.Check(err)
+
+		//debug print
+		fmt.Println(foodType, foodDistance, foodPrice)
+		url := place.ProduceQueryString(foodType, floatFoodDistance, floatFoodPrice)
+		resp, err := http.Get(url) //url is globally located in config.go
+
+		myerror.Check(err)
+		defer resp.Body.Close()
+
+		var myPlace place.Places = LoadPlaces(resp.Body)
+		place.PrintSuggestions(myPlace)
 
 	})
 
