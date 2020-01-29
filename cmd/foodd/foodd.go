ef768eebcc40714d76de01bec842c53fd3bd2d5f
ken343 <kmcdole21@gmail.com>
2020-01-29 03:09:19 -0600

    Must Get r.Form.Get to return non-empty items.

--- cmd/foodd/foodd.go
+++ cmd/foodd/foodd.go
@@ -14,25 +14,35 @@ import (
 func main() {
 	fmt.Println("Server is running on port 8080...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/chow", func(w http.ResponseWriter, r *http.Request) {
 		//Retrieve food type, distance and price from client application.
 
 		r.ParseForm()
-		foodType := r.Form.Get("food")
-		foodDistance := r.Form.Get("distance")
-		foodPrice := r.Form.Get("pricelevel")
-		floatFoodDistance, err := strconv.ParseFloat(foodDistance, 64)
+		myValues := r.URL.Query()
+
+		fmt.Println("Passed in URL: ", r.URL.RawQuery)
+		// Form.Get() is giving hard errors for distance and pricelevel.
+		// foodType := r.Form.Get("food")
+		// foodDistance := r.Form.Get("distance")
+		// foodPrice := r.Form.Get("pricelevel")
+		foodType := myValues.Get("food")
+		foodDistance := myValues.Get("distance")
+		foodPrice := myValues.Get("pricelevel")
+		fmt.Println("Before string conversions: foodType=", foodType, "foodDistance=", foodDistance, "foodPrice=", foodPrice)
+		floatFoodDistance, err := strconv.ParseFloat("11.11", 64)
 		myerror.Check(err)
-		floatFoodPrice, err := strconv.ParseFloat(foodPrice, 64)
+		floatFoodPrice, err := strconv.ParseFloat("30.2", 64)
 		myerror.Check(err)
 
 		//debug print
-		fmt.Println(foodType, foodDistance, foodPrice)
+		// Retrieve Place from API
+		fmt.Println("After Parse Form", foodType, foodDistance, foodPrice)
 		url := place.ProduceQueryString(foodType, floatFoodDistance, floatFoodPrice)
 		resp, err := http.Get(url) //url is globally located in config.go
 		myerror.Check(err)
 		defer resp.Body.Close()
 
+		// Decode response and "Re-code it back to the client"
 		var myPlace place.Places = LoadPlaces(resp.Body)
 
 		w.Header().Set("Content-Type", "application/json")
